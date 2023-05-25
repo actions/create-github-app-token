f048b0d8eecdf325a4c6175b2001b9e10e077961
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2023-05-25 16:38:11 -0700

    Replace octokit with @octokit/auth-app and @octokit/request

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // @ts-check
 
 import core from "@actions/core";
-import { App } from "octokit";
+import { createAppAuth } from "@octokit/auth-app";
+import { request } from "@octokit/request";
 
 // we export the `main` function for testing.
 // Call `main()` directly if this file is the entry point
@@ -12,53 +13,69 @@ if (import.meta.url.endsWith(process.argv[1])) {
     );
   }
 
-  const app = new App({
-    appId: core.getInput("app_id"),
-    privateKey: core.getInput("private_key"),
-  });
+  const appId = core.getInput("app_id");
+  const privateKey = core.getInput("private_key");
 
   const repository = process.env.GITHUB_REPOSITORY;
 
-  main(app, repository, core).catch((error) => {
-    console.error(error);
-    core.setFailed(error.message);
-  });
+  main(appId, privateKey, repository, core, createAppAuth, request).catch(
+    (error) => {
+      console.error(error);
+      core.setFailed(error.message);
+    }
+  );
 }
 
 /**
- * @param {App} app
+ * @param {string} appId
+ * @param {string} privateKey
  * @param {string} repository
  * @param {core} core
+ * @param {createAppAuth} createAppAuth
+ * @param {request} request
  */
-export async function main(app, repository, core) {
+export async function main(
+  appId,
+  privateKey,
+  repository,
+  core,
+  createAppAuth,
+  request
+) {
   // Get owner and repo name from GITHUB_REPOSITORY
   const [owner, repo] = repository.split("/");
 
+  const auth = createAppAuth({
+    appId,
+    privateKey,
+  });
+
+  const appAuthentication = await auth({
+    type: "app",
+  });
+
   // Get the installation ID
   // https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#get-a-repository-installation-for-the-authenticated-app
-  const { data: installation } = await app.octokit.request(
+  const { data: installation } = await request(
     "GET /repos/{owner}/{repo}/installation",
     {
       owner,
       repo,
+      headers: {
+        authorization: `bearer ${appAuthentication.token}`,
+      },
     }
   );
 
-  // Create Octokit instance
-  const octokit = await app.getInstallationOctokit(installation.id);
-
   // Create a new installation token
-  const authentication = await octokit.auth({
+  const authentication = await auth({
     type: "installation",
     installationId: installation.id,
     repositoryNames: [repo],
   });
 
-  // @ts-expect-error
-  core.setOutput("token", authentication.token)
+  core.setOutput("token", authentication.token);
 
   // Make token accessible to post function (so we can invalidate it)
-  // @ts-expect-error
   core.saveState("token", authentication.token);
-
 }
