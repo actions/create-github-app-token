0d62a31882e77f6d2327747b99aba29800932745
Clay Miller <clay@smockle.com>
2023-10-04 11:14:03 -0400

    Revert "test: Add tests to verify 'main' overrides 'request'’s default 'baseUrl' only when 'GITHUB_API_URL' is set"
    This reverts commit 9d872596a47c5a37701c0eb06693763733b60519.

--- tests/index.js
+++ tests/index.js
@@ -1,24 +1,13 @@
-import { readFileSync, readdirSync } from "node:fs";
-import * as url from "node:url";
+import { readdirSync } from "node:fs";
 
 import { execa } from "execa";
 import test from "ava";
-
-// If ava is configured to use node arguments, use them when this script runs node
-const nodeArguments =
-  readFileSync(
-    url.fileURLToPath(new URL("../package.json", import.meta.url)),
-    "utf8"
-  )?.ava?.nodeArguments ?? [];
 
 const tests = readdirSync("tests").filter((file) => file.endsWith(".test.js"));
 
 for (const file of tests) {
   test(file, async (t) => {
-    const { stderr, stdout } = await execa("node", [
-      ...nodeArguments,
-      `tests/${file}`,
-    ]);
+    const { stderr, stdout } = await execa("node", [`tests/${file}`]);
     t.snapshot(stderr, "stderr");
     t.snapshot(stdout, "stdout");
   });
