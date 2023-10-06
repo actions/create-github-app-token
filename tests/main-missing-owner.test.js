fabe5404a563d7c338e6019fb78642619983b66d
Clay Miller <clay@smockle.com>
2023-10-06 15:04:00 -0400

    fix: Remove async IIFE wrapper; use top-level 'await'

--- tests/main-missing-owner.test.js
+++ tests/main-missing-owner.test.js
@@ -2,10 +2,8 @@ process.env.GITHUB_REPOSITORY = "actions/create-github-app-token";
 delete process.env.GITHUB_REPOSITORY_OWNER;
 
 // Verify `main` exits with an error when `GITHUB_REPOSITORY_OWNER` is missing.
-(async () => {
-  try {
-    await import("../main.js");
-  } catch (error) {
-    console.error(error.message);
-  }
-})();
+try {
+  await import("../main.js");
+} catch (error) {
+  console.error(error.message);
+}
