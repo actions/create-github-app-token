0c014070f93045fed9b48f568f28b2f1cca37088
Josh Gross <joshmgross@github.com>
2024-01-19 10:45:12 -0500

    fix(revocation): avoid revoking expired tokens and fail gracefully (#95)
    Fixes #72
    
    If an Actions job is long enough, more than an hour can pass between
    creating and revoking the App token in the post-job clean up step. Since
    the token itself is used to authenticate with the revoke API, an expired
    token will fail to be revoked.
    
    This PR saves the token expiration in the actions state and uses that in
    the post step to determine if the token can be revoked. I've also added
    error handling to the revoke token API call, as it's unlikely that users
    would want their job to fail if the token can't be revoked.

--- tests/post-token-expired.test.js
+++ tests/post-token-expired.test.js
@@ -4,8 +4,8 @@ import { MockAgent, setGlobalDispatcher } from "undici";
 // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#sending-values-to-the-pre-and-post-actions
 process.env.STATE_token = "secret123";
 
-// 1 hour in the future, not expired
-process.env.STATE_expiresAt = new Date(Date.now() + 1000 * 60 * 60).toISOString();
+// 1 hour in the past, expired
+process.env.STATE_expiresAt = new Date(Date.now() - 1000 * 60 * 60).toISOString();
 
 const mockAgent = new MockAgent();
 
