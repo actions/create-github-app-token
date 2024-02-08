22ed2d593487e18ab912b5c6e8c5895655b4e691
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2024-02-08 15:34:02 -0800

    Update main.js

--- tests/main.js
+++ tests/main.js
@@ -6,7 +6,7 @@ export const DEFAULT_ENV = {
   GITHUB_REPOSITORY_OWNER: "actions",
   GITHUB_REPOSITORY: "actions/create-github-app-token",
   // inputs are set as environment variables with the prefix INPUT_
-  // https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#example-specifying-inputs
+  // https://docs.github.com/actions/creating-actions/metadata-syntax-for-github-actions#example-specifying-inputs
   "INPUT_GITHUB-API-URL": "https://api.github.com",
   "INPUT_APP-ID": "123456",
   // This key is invalidated. It’s from https://github.com/octokit/auth-app.js/issues/465#issuecomment-1564998327.
