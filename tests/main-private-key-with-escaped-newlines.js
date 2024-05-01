35f361572f24ea7f328c451c3ed947a813a87ea5
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-05-01 08:50:50 -0700

    Update tests/main-private-key-with-escaped-newlines.js

--- tests/main-private-key-with-escaped-newlines.js
+++ tests/main-private-key-with-escaped-newlines.js
@@ -1,6 +1,6 @@
 import { test, DEFAULT_ENV } from "./main.js";
 
-// Verify `main` successfully obtains a token when the `owner` input is not set, but the `repositories` input is set.
+// Verify `main` works correctly when `private-key` input has escaped newlines
 await test(() => {
   process.env['INPUT_PRIVATE-KEY'] = DEFAULT_ENV.PRIVATE_KEY.replace(/\n/g, '\\n')
 });
