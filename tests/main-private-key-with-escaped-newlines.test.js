36d74b372abb0c2309135ed8e138d9e9fdf8d499
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2024-08-08 17:42:55 -0700

    Update main-private-key-with-escaped-newlines.test.js

--- tests/main-private-key-with-escaped-newlines.test.js
+++ tests/main-private-key-with-escaped-newlines.test.js
@@ -2,5 +2,8 @@ import { DEFAULT_ENV, test } from "./main.js";
 
 // Verify `main` works correctly when `private-key` input has escaped newlines
 await test(() => {
-  process.env['INPUT_PRIVATE-KEY'] = DEFAULT_ENV.PRIVATE_KEY.replace(/\n/g, '\\n')
+  process.env["INPUT_PRIVATE-KEY"] = DEFAULT_ENV["INPUT_PRIVATE-KEY"].replace(
+    /\n/g,
+    "\\n"
+  );
 });
