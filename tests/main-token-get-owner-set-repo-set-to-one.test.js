f3530fa5af83a4061137b6158b77648f96d1564b
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2024-08-22 16:11:09 -0700

    Remove owner from repositories input in tests

--- tests/main-token-get-owner-set-repo-set-to-one.test.js
+++ tests/main-token-get-owner-set-repo-set-to-one.test.js
@@ -3,5 +3,6 @@ import { test } from "./main.js";
 // Verify `main` successfully obtains a token when the `owner` and `repositories` inputs are set (and the latter is a single repo).
 await test(() => {
   process.env.INPUT_OWNER = process.env.GITHUB_REPOSITORY_OWNER;
-  process.env.INPUT_REPOSITORIES = process.env.GITHUB_REPOSITORY;
+  const currentRepoName = process.env.GITHUB_REPOSITORY.split("/")[1];
+  process.env.INPUT_REPOSITORIES = currentRepoName;
 });
