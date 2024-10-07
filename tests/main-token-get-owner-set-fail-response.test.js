14c9041dc1cedc8c61715577bd2da13d0e8bd8d1
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2024-10-07 12:49:56 -0700

    Update tests to reflect changes to API requests

--- tests/main-token-get-owner-set-fail-response.test.js
+++ tests/main-token-get-owner-set-fail-response.test.js
@@ -1,6 +1,6 @@
 import { test } from "./main.js";
 
-// Verify `main` successfully obtains a token when the `owner` input is set (to a user), but the `repositories` input isn’t set.
+// Verify retries work when getting a token for a user or organization fails on the first attempt.
 await test((mockPool) => {
   process.env.INPUT_OWNER = "smockle";
   delete process.env.INPUT_REPOSITORIES;
@@ -18,9 +18,20 @@ await test((mockPool) => {
         // Intentionally omitting the `authorization` header, since JWT creation is not idempotent.
       },
     })
+    .reply(500, "GitHub API not available");
+  mockPool
+    .intercept({
+      path: `/users/${process.env.INPUT_OWNER}/installation`,
+      method: "GET",
+      headers: {
+        accept: "application/vnd.github.v3+json",
+        "user-agent": "actions/create-github-app-token",
+        // Intentionally omitting the `authorization` header, since JWT creation is not idempotent.
+      },
+    })
     .reply(
       200,
-      { id: mockInstallationId, "app_slug": mockAppSlug },
+      { id: mockInstallationId, app_slug: mockAppSlug },
       { headers: { "content-type": "application/json" } }
     );
 });
--- tests/main-token-get-owner-set-repo-unset.test.js
+++ tests/main-token-get-owner-set-repo-unset.test.js
@@ -1,8 +1,8 @@
 import { test } from "./main.js";
 
-// Verify retries work when getting a token for a user or organization fails on the first attempt.
+// Verify `main` successfully obtains a token when the `owner` input is set, and the `repositories` input isn’t set.
 await test((mockPool) => {
-  process.env.INPUT_OWNER = "smockle";
+  process.env.INPUT_OWNER = process.env.GITHUB_REPOSITORY_OWNER;
   delete process.env.INPUT_REPOSITORIES;
 
   // Mock installation ID and app slug request
@@ -18,20 +18,9 @@ await test((mockPool) => {
         // Intentionally omitting the `authorization` header, since JWT creation is not idempotent.
       },
     })
-    .reply(500, "GitHub API not available");
-  mockPool
-    .intercept({
-      path: `/users/${process.env.INPUT_OWNER}/installation`,
-      method: "GET",
-      headers: {
-        accept: "application/vnd.github.v3+json",
-        "user-agent": "actions/create-github-app-token",
-        // Intentionally omitting the `authorization` header, since JWT creation is not idempotent.
-      },
-    })
     .reply(
       200,
-      { id: mockInstallationId, "app_slug": mockAppSlug },
+      { id: mockInstallationId, app_slug: mockAppSlug },
       { headers: { "content-type": "application/json" } }
     );
 });
