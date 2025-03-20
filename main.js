44c52c73d7d439511f4d65dd89f6809283411d22
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-03-20 11:12:48 -0700

    WIP implementation, not yet tested

--- main.js
+++ main.js
@@ -35,37 +35,32 @@ const skipTokenRevoke = Boolean(
   core.getInput("skip-token-revoke") || core.getInput("skip_token_revoke"),
 );
 
-// TODO: get all permissions from inputs. Find all environment variables with `INPUT_PERMISSION_` prefix
-//       and parse them into an object: {[permission_name]: [permission_access]}
-//       e.g.
-//
-//         with:
-//           permission-organization-administration: write
-//
-//       will set the environment variable
-//
-//       ` INPUT_PERMISSION_ORGANIZATION_ADMINISTRATION=write`
-//
-// ``    which should be parsed into this permissions object
-//
-//         { "organization-administration": "write" }`
-//
-//        Ideally we verify all permission variables and their values, and log a warning if one is unknown.
-//        This should be a follow up issue though.
-//
-//       Code could look something like this
-//
-//         const permissions = getPermissionsFromInputs(process.env)
-//
-//       Where permissions is either undefined (unlimited) or a `Record<string, string>`. We should pass it after `repositories` to `main()`
-//
 // @see https://docs.github.com/en/actions/sharing-automations/creating-actions/metadata-syntax-for-github-actions#inputs
+function getPermissionsFromInputs(env) {
+  return Object.entries(env).reduce((permissions, [key, value]) => {
+    if (!key.startsWith("INPUT_PERMISSION_")) return permissions;
+
+    const permission = key.slice("INPUT_PERMISSION_".length).toLowerCase();
+    if (permissions === undefined) {
+      return { [permission]: value };
+    }
+
+    return {
+      // @ts-expect-error - needs to be typed correctly
+      ...permissions,
+      [permission]: value,
+    };
+  }, undefined);
+}
+
+const permissions = getPermissionsFromInputs(process.env);
 
 main(
   appId,
   privateKey,
   owner,
   repositories,
+  permissions,
   core,
   createAppAuth,
   request,
