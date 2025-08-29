7830ec20d5d358f39cda3d2629e87e5de23cac4f
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-08-29 11:35:45 -0700

    spawn subprocess for `main.js` if enabled using http proxy env variables

--- main.js
+++ main.js
@@ -15,7 +15,31 @@ if (!process.env.GITHUB_REPOSITORY_OWNER) {
   throw new Error("GITHUB_REPOSITORY_OWNER missing, must be set to '<owner>'");
 }
 
+import { spawn } from "node:child_process";
+// export for testing
+export { spawn } from "node:child_process";
+
 async function run() {
+  // spawn a child process if proxy is set
+  const httpProxyEnvVars = [
+    "https_proxy",
+    "HTTPS_PROXY",
+    "http_proxy",
+    "HTTP_PROXY",
+  ];
+  const nodeHasProxySupportEnabled = process.env.NODE_USE_ENV_PROXY === "1";
+  const shouldUseProxy = httpProxyEnvVars.some((v) => process.env[v]);
+
+  if (!nodeHasProxySupportEnabled && shouldUseProxy) {
+    // spawn itself with NODE_USE_ENV_PROXY=1
+    const child = spawn(process.execPath, process.argv.slice(1), {
+      env: { ...process.env, NODE_USE_ENV_PROXY: "1" },
+      stdio: "inherit",
+    });
+    child.on("exit", (code) => process.exit(code));
+    return;
+  }
+
   const appId = core.getInput("app-id");
   const privateKey = core.getInput("private-key");
   const owner = core.getInput("owner");
